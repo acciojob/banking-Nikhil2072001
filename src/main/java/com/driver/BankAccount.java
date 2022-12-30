002d43cb68760d2fa2faf5f11c0e045b6a501506
Nikhil2072001 <116430126+Nikhil2072001@users.noreply.github.com>
2022-12-30 21:22:57 +0530

    Submitted

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -7,25 +7,64 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
-
+this.name=name;
+this.balance=balance;
+this.minBalance=minBalance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-
-        return null;
+int sumDummy = 0;
+int temp = digits;
+while(temp>0){
+    sumDummy+=(temp%10);
+ temp=temp/10;}
+if(sumDummy==sum){
+    String s= Integer.toString(sumDummy);
+    return s;
+}else{
+    String s = "Account Number can not be generated";
+     throw new Exception(s);
+}
     }
 
     public void deposit(double amount) {
         //add amount to balance
-
+this.balance=this.balance+amount;
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+if(balance-amount>0 && balance-amount<minBalance){
+    String s = "Insufficient Balance";
+    throw new Exception(s);
+}
+balance-=amount;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
+    }
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,10 +1,20 @@
 package com.driver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+super(name,balance,5000);
+        this.tradeLicenseId= tradeLicenseId;
+if(balance<5000){
+    String s = "Insufficient Balance";
+    throw new Exception(s);
+}
+
 
     }
 
@@ -13,7 +23,45 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
-
+if(isValid(tradeLicenseId))
+    return;
+else{
+    solve(tradeLicenseId,"");
+    for(String s:ans){
+        if(isValid(s)){
+            this.tradeLicenseId=s;
+            return;
+        }
+    }
+    String e = "Valid License can not be generated";
+    throw new Exception(e);
+}
+    }
+    List<String> ans = new ArrayList<>();
+    public void solve(String s,String t){
+        if(s.length()==0){
+            if(!ans.contains(t))
+                ans.add(t);
+            return;
+        }
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            String l = s.substring(0,i);
+            String r = s.substring(i+1);
+            String a = l+r;
+            solve(a,t+c);
+        }
+    }
+    public boolean isValid(String s){
+        int n = s.length();
+        for(int i=0;i<n-1;i++)
+        {
+            char c1=s.charAt(i);
+            char c2 = s.charAt(i+1);
+            if(c1==c2)
+                return  false;
+        }
+        return true;
     }
 
 }
